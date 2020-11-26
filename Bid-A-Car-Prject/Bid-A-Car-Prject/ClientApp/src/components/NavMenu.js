2af5f56c98e831bb174e1bedb025ead73552e889
UmmerZ <ummer.zaman88@gmail.com>
2020-11-25 19:09:29 -0700

    Made changes to nav Menu for new Navigation Items

--- Bid-A-Car-Prject/Bid-A-Car-Prject/ClientApp/src/components/NavMenu.js
+++ Bid-A-Car-Prject/Bid-A-Car-Prject/ClientApp/src/components/NavMenu.js
@@ -34,10 +34,10 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/create-listing">Create A Listing</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/get-listings">Listing</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
