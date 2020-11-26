003777efcde493c5ca8fe14e1046831a80ebc6ea
UmmerZ <ummer.zaman88@gmail.com>
2020-11-25 17:58:17 -0700

    Added User Table in Models Folder

---
+++ Bid-A-Car-Prject/Bid-A-Car-Prject/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{ [Table("User")]
+    public class User
+    {
+        public User()
+        {
+            Vehicles = new HashSet<Vehicle>();
+        }
+       
+
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string UserName { get; set; }
+
+        [Column(TypeName = "int(12)")]
+        public int PhoneNumber { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string StreetAdress { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string City { get; set; }
+
+         [Column(TypeName = "varchar(8)")]
+        public string PostalCode { get; set; }
+
+
+        
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
+
+
+    }
+}
