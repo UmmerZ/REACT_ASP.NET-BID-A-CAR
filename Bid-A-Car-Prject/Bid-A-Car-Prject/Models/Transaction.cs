953bbcc450f9c6e392d5369b2ef862b345b5bc5c
UmmerZ <ummer.zaman88@gmail.com>
2020-11-25 18:00:53 -0700

    Added Transactions Table Code First

---
+++ Bid-A-Car-Prject/Bid-A-Car-Prject/Models/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    [Table("Transaction")]
+    public class Transaction
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionID { get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int SellerID { get; set; }
+
+       
+
+        [Column(TypeName = "int(15)")]
+        public int SaleAmount { get; set; }
+
+        [ForeignKey(nameof(SellerID))]
+        [InverseProperty(nameof(Models.User.Transactions))]
+         public virtual User User { get; set; }
+
+
+        
+    }
+}
