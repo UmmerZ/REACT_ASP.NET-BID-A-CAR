4a779c0b8503ee51a2d7c8f4adae63077c1f2ddf
UmmerZ <ummer.zaman88@gmail.com>
2020-11-25 18:39:45 -0700

    Added Vehicle Controller

---
+++ Bid-A-Car-Prject/Bid-A-Car-Prject/Controllers/VehicleController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            ViewBag.Products = GetListings();
+            return View();
+        }
+        public IActionResult Details(string id)
+        {
+            ViewBag.Product = GetListingByID(id);
+            return View();
+        }
+        public Vehicle GetListingByID(string id)
+        {
+            Vehicle result;
+            using (SaleContext context = new SaleContext())
+            {
+               result = context.Vehicles.Where(x => x.VehicleID == int.Parse(id)).Single();
+
+            }
+
+            return result;
+        }
+        //public IActionResult Create(string id, string make, string model, string kms, string year, string description, string userID)
+        //{
+
+        //    try
+        //    {
+        //        CreateListing(id, make, model, kms, year, description, userID);
+        //        ViewBag.Message = $"Product Created";
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        ViewBag.Error = e;
+        //        ViewBag.Message = $"Error with Validation:  {e.Message}";
+        //    }
+
+        //    return View();
+        //}
+        public Vehicle CreateListing(string id, string make, string model, string kms, string year, string description, string userID)
+        {
+            
+            using (SaleContext context = new SaleContext())
+            {
+
+                Vehicle newListing = new Vehicle()
+                {
+
+                    VehicleID = int.Parse(id),
+                    Make = make.Trim(),
+                    Model = model.Trim(),
+                    Kilometers = int.Parse(kms),
+                   Year = DateTime.Parse(year),
+                   Description = description.Trim(),
+                   UserID = int.Parse(userID)
+                   
+
+                };
+                context.Vehicles.Add(newListing);
+                context.SaveChanges();
+                return newListing;
+            }
+        }
+        public List<Vehicle> GetListings()
+        {
+            List<Vehicle> results;
+            using (SaleContext context = new SaleContext())
+            {
+                results = context.Vehicles.ToList();
+            }
+            return results;
+        }
+    }
+}
