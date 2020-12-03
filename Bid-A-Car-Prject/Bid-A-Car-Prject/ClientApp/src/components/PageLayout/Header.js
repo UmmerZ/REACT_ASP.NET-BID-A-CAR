c077408118882aee0c32fe2bf74504387d859b81
UmmerZ <ummer.zaman88@gmail.com>
2020-12-02 23:46:21 -0700

    Added Header

---
+++ Bid-A-Car-Prject/Bid-A-Car-Prject/ClientApp/src/components/PageLayout/Header.js
@@ -0,0 +1,17 @@
+﻿import React from "react";
+
+
+const Header = () => {
+
+    return (
+        <>
+            <header>
+
+                <h1 className="logo">Bid<strong className="logo-cars">Cars</strong></h1>
+                <Nav />
+
+            </header>
+        </>
+    );
+};
+export default Header;
